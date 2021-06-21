8e77d420ee51de09fc9891ec3c428e5ae894f579
Yuluer <73686123+Yuluer@users.noreply.github.com>
2021-06-22 01:44:16 +0800

    user login register

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask,render_template#从flask包中导入Flask类
 from config import Config
+from flask_login import LoginManager
 
 from flask_sqlalchemy import SQLAlchemy#从包中导入类
 from flask_migrate import Migrate
@@ -11,6 +12,9 @@ ath.config.from_object(Config)
 
 db = SQLAlchemy(ath)#数据库对象
 migrate = Migrate(ath, db)#迁移引擎对象
+
+login = LoginManager(ath)
+login.login_view='login'
 
 from app import routes,models#从app包中导入模块routes#将Flask类的实例 赋值给名为 app 的变量。这个实例成为app包的成员。
 
--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,17 @@
-from app import ath
-from flask import render_template, flash, redirect, url_for
-from app.forms import LoginForm
+from werkzeug.urls import url_parse
+
+from app import ath,db
+from flask import render_template, flash, redirect, url_for,request
+from app.forms import LoginForm, RegistrationForm
+from flask_login import current_user, login_user, logout_user, login_required
+from app.models import User
 
 
 @ath.route('/')
-@ath.route('/index',methods=['GET'])
+@ath.route('/index',methods=['GET','POST'])
+@login_required
 def index():
-	user = {'username': 'Miguel'}
+
 	posts = [  # 创建一个列表：帖子。里面元素是两个字典，每个字典里元素还是字典，分别作者、帖子内容。
 		{
 			'author': {'username': 'John'},
@@ -17,26 +22,42 @@ def index():
 			'body': 'The Avengers movie was so cool!'
 		}
 	]
-	return render_template('index.html',  user=user,posts=posts)
+	return render_template('index.html',posts=posts)
 
 @ath.route('/login',methods=['GET','POST'])
 def login():
-	form = LoginForm()#表单实例化对象
-	if form.validate_on_submit():
-		flash('Login requested for user {},remember_me={}'.format(form.username.data, form.remember_me.data))
+	if current_user.is_authenticated:
 		return redirect(url_for('index'))
-	return render_template('login.html', title='Sign In', form=form)
+	form = LoginForm()
+	if form.validate_on_submit():
+		user = User.query.filter_by(username=form.username.data).first()
+		if user is None or not user.check_password(form.password.data):
+			flash('Invalid username or password')
+			return redirect(url_for('login'))
+		login_user(user, remember=form.remember_me.data)
 
-# @ath.route('/register', methods=['GET', 'POST'])
-# def register():
-#     if current_user.is_authenticated:
-#         return redirect(url_for('index'))
-#     form = RegistrationForm()
-#     if form.validate_on_submit():
-#         user = User(username=form.username.data, email=form.email.data)
-#         user.set_password(form.password.data)
-#         db.session.add(user)
-#         db.session.commit()
-#         flash('Congratulations, you are now a registered user!')
-#         return redirect(url_for('login'))
-#     return render_template('register.html', title='Register', form=form)
+		# 重定向到 next 页面
+		next_page = request.args.get('next')
+		if not next_page or url_parse(next_page).netloc != '':
+			next_page = url_for('index')
+		return redirect(next_page)
+	return render_template('login.html',title = 'login',form = form)
+
+@ath.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@ath.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
