34d8d7f965fab99eff4ec3387a15a0dd349d92eb
Yuluer <73686123+Yuluer@users.noreply.github.com>
2021-06-21 21:22:19 +0800

    forms finished

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask,render_template#从flask包中导入Flask类
+from config import Config
 
 ath = Flask(__name__)
-print('等会谁（哪个包或模块）在使用我：',__name__)
+# print('等会谁（哪个包或模块）在使用我：',__name__)
+ath.config.from_object(Config)
+
 
 from app import routes#从app包中导入模块routes#将Flask类的实例 赋值给名为 app 的变量。这个实例成为app包的成员。
 
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,26 @@
 from app import ath
-from flask import render_template
-@ath.route('/',methods=['GET'])
+from flask import render_template, flash, redirect, url_for
+from app.forms import LoginForm
+
+@ath.route('/index',methods=['GET'])
 def index():
-	return "'render_template('index.html')'"
+	user = {'username': 'Miguel'}
+	posts = [  # 创建一个列表：帖子。里面元素是两个字典，每个字典里元素还是字典，分别作者、帖子内容。
+		{
+			'author': {'username': 'John'},
+			'body': 'Beautiful day in Portland!'
+		},
+		{
+			'author': {'username': 'Susan'},
+			'body': 'The Avengers movie was so cool!'
+		}
+	]
+	return render_template('index.html',  user=user,posts=posts)
+
+@ath.route('/login',methods=['GET','POST'])
+def login():
+	form = LoginForm()#表单实例化对象
+	if form.validate_on_submit():
+		flash('Login requested for user {},remember_me={}'.format(form.username.data, form.remember_me.data))
+		return redirect(url_for('index'))
+	return render_template('login.html', title='Sign In', form=form)
