45d220f39cfec1f718746fb60a70cffda9d92bd4
Yuluer <73686123+Yuluer@users.noreply.github.com>
2021-06-22 21:57:54 +0800

    errors_finished_no_logger

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,9 @@ from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy#从包中导入类
 from flask_migrate import Migrate
 
+from logging.handlers import RotatingFileHandler
+import os
+
 ath = Flask(__name__)
 # print('等会谁（哪个包或模块）在使用我：',__name__)
 ath.config.from_object(Config)
@@ -16,5 +19,5 @@ migrate = Migrate(ath, db)#迁移引擎对象
 login = LoginManager(ath)
 login.login_view='login'
 
-from app import routes,models#从app包中导入模块routes#将Flask类的实例 赋值给名为 app 的变量。这个实例成为app包的成员。
+from app import routes,models,errors#从app包中导入模块routes#将Flask类的实例 赋值给名为 app 的变量。这个实例成为app包的成员。
 
--- app/forms.py
+++ app/forms.py
@@ -6,10 +6,10 @@ from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired, Length
 
 class LoginForm(FlaskForm):
-	username = StringField('Username', validators=[DataRequired()])
-	password = PasswordField('Password', validators=[DataRequired()])
-	remember_me = BooleanField('Remember Me')
-	submit = SubmitField('Sign In')
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
 
 
 class RegistrationForm(FlaskForm):
@@ -32,6 +32,16 @@ class RegistrationForm(FlaskForm):
 
 
 class EditProfileForm(FlaskForm):
-	username = StringField('Username', validators=[DataRequired()])
-	about_me = TextAreaField('About_me', validators=[Length(min=0, max=140)])
-	submit = SubmitField('Submit')
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')
+    #验证用户名
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=self.username.data).first()
+            if user is not None:
+                raise ValidationError('Please use a different username.')
