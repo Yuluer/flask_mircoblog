52e76cb980f4292be4b3da96c53583e481f6a571
Yuluer <73686123+Yuluer@users.noreply.github.com>
2021-06-23 20:41:11 +0800

    post_pagination_finished

--- app/models.py
+++ app/models.py
@@ -18,15 +18,15 @@ def load_user(id):
     return User.query.get(int(id))
 
 class User(UserMixin, db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	username = db.Column(db.String(64), index=True, unique=True)
-	email = db.Column(db.String(120), index=True, unique=True)
-	password_hash = db.Column(db.String(128))
-	posts = db.relationship('Post', backref='author', lazy='dynamic')
-	about_me = db.Column(db.String(140))
-	last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
-	followed = db.relationship(
+    followed = db.relationship(
 		'User',
 		secondary=followers,
 		primaryjoin=(followers.c.follower_id == id),
@@ -35,45 +35,45 @@ class User(UserMixin, db.Model):
 		lazy='dynamic'
 	)
 
-	def __repr__(self):
-		return '<User {}>'.format(self.username)
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
 
-	def __repr__(self):
-		return '<User {}>'.format(self.username)
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
 
-	def set_password(self, password):
-		self.password_hash = generate_password_hash(password)
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
 
-	def check_password(self, password):
-		return check_password_hash(self.password_hash, password)
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
-	def avatar(self, size):
-		digest = md5(self.email.lower().encode('utf-8')).hexdigest()
-		return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
 			digest, size)
 
-	def follow(self, user):
-		if not self.is_following(user):
-			self.followed.append(user)
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
 
-	def unfollow(self, user):
-		if self.is_following(user):
-			self.followed.remove(user)
+    def unfollow(self, user):
+        if self.is_following(user):
+             self.followed.remove(user)
 
-	def is_following(self, user):
-		return self.followed.filter(followers.c.followed_id == user.id).count() > 0
+    def is_following(self, user):
+        return self.followed.filter(followers.c.followed_id == user.id).count() > 0
 
-	def followed_posts(self):
-		followed = Post.query.join(followers, (followers.c.followed_id == Post.user_id)).filter(
+    def followed_posts(self):
+        followed = Post.query.join(followers, (followers.c.followed_id == Post.user_id)).filter(
 			followers.c.follower_id == self.id)
-		own = Post.query.filter_by(user_id=self.id)
-		return followed.union(own).order_by(Post.timestamp.desc())#union连接自己和关注者的帖子 按照时间排序
+        own = Post.query.filter_by(user_id=self.id)
+        return followed.union(own).order_by(Post.timestamp.desc())#union连接自己和关注者的帖子 按照时间排序
 
 class Post(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	body = db.Column(db.String(140))
-	timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-	user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-	def __repr__(self):
-		return '<Post {}'.format(self.body)
+    def __repr__(self):
+        return '<Post {}'.format(self.body)
--- app/routes.py
+++ app/routes.py
@@ -2,9 +2,9 @@ from werkzeug.urls import url_parse
 
 from app import ath,db
 from flask import render_template, flash, redirect, url_for,request
-from app.forms import LoginForm, RegistrationForm, EditProfileForm, EmptyForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, EmptyForm, PostForm
 from flask_login import current_user, login_user, logout_user, login_required
-from app.models import User
+from app.models import User, Post
 from datetime import datetime
 
 
@@ -16,19 +16,22 @@ def before_request():
 
 @ath.route('/')
 @ath.route('/index',methods=['GET','POST'])
+@login_required
 def index():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
+    page = request.args.get('page', 1, type=int)
+    posts = current_user.followed_posts().paginate(page, ath.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('index', page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('index', page=posts.prev_num) if posts.has_prev else None
+    return render_template('index.html', title='Home Page', form=form, posts=posts.items, next_url=next_url,
+                           prev_url=prev_url)
 
-	posts = [  # 创建一个列表：帖子。里面元素是两个字典，每个字典里元素还是字典，分别作者、帖子内容。
-		{
-			'author': {'username': 'John'},
-			'body': 'Beautiful day in Portland!'
-		},
-		{
-			'author': {'username': 'Susan'},
-			'body': 'The Avengers movie was so cool!'
-		}
-	]
-	return render_template('index.html',posts=posts)
 
 @ath.route('/login',methods=['GET','POST'])
 def login():
@@ -72,11 +75,11 @@ def register():
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    posts = [
-        {'author':user, 'body':'Test post #1'},
-        {'author':user, 'body':'Test post #2'}
-    ]
-    return render_template('user.html', user=user, posts=posts)
+    page = request.args.get('page', 1, type=int)
+    posts = user.posts.order_by(Post.timestamp.desc()).paginate(page, ath.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('user', username=user.username, page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('user', username=user.username, page=posts.prev_num) if posts.has_prev else None
+    return render_template('user.html', user=user, posts=posts.items, next_url=next_url, prev_url=prev_url)
 
 @ath.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
@@ -124,3 +127,11 @@ def unfollow(username):
         db.session.commit()
         flash('You are not following {}.'.format(username))
         return redirect(url_for('user', username=username))
+
+@ath.route('/explore')
+def explore():
+    page = request.args.get('page', 1, type=int)
+    posts = current_user.followed_posts().paginate(page, ath.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('explore', page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('explore', page=posts.prev_num) if posts.has_prev else None
+    return render_template('index.html', title='Explore', posts=posts.items, next_url=next_url, prev_url=prev_url)
